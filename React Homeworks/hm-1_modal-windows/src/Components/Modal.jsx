5bb796405bbf08ab8ecace4dfe414521641b6f69
Evgeniy Shigartsov <evgeniy.shigartsov2@gmail.com>
2021-01-05 23:20:30 +0200

    change handler for modal wrapper

--- React Homeworks/hm-1_modal-windows/src/Components/Modal.jsx
+++ React Homeworks/hm-1_modal-windows/src/Components/Modal.jsx
@@ -8,19 +8,23 @@ class Modal extends React.Component {
         super(props)
         this.closeBtn = <Button classList="close-btn" text="X" backgroundColor="#ccc" onClick={() => this.props.setActive(false)} />
     }
+    modalWrapperHandler = (e) => {
+        if (!e.target.classList.contains('modal-wrapper')) return
+        this.props.setActive(false)
+    }
     render() {
         return (
-            <div className={this.props.active ? 'modal-wrapper' : 'modal-wrapper display-none'} onClick={() => this.props.setActive(false)}>
-                <div className="modal-pop-up" onClick={(e) => e.stopPropagation()}>
+            <div className={this.props.active ? 'modal-wrapper' : 'modal-wrapper display-none'} onClick={this.modalWrapperHandler}>
+                <div className="modal-pop-up">
                     <div className="modal-content">
-                        {this.props.closeButton ? this.closeBtn : null}
+                        {this.props.closeButton ? this.closeBtn : ''}
                         <h3 className="modal-header">{this.props.header}</h3>
                         <div className="modal-body">
                             <p>{this.props.text}</p>
                         </div>
                         <div className="modal-footer">
-                            <Button classList="btn modal-btn--two" backgroundColor="#17a2b8" text={this.props.btnText.cancel} onClick={() => this.props.setActive(false)} />
-                            <Button classList="btn modal-btn--one" backgroundColor="#dc3545" text={this.props.btnText.ok} onClick={() => this.props.setActive(false)} />
+                            <Button classList="btn" backgroundColor="#17a2b8" text={this.props.btnText.cancel} onClick={() => this.props.setActive(false)} />
+                            <Button classList="btn" backgroundColor="#dc3545" text={this.props.btnText.ok} onClick={() => this.props.setActive(false)} />
                         </div>
                     </div>
                 </div>
