a37ef18c10a128f79e54581e3b2bf45d329a66a2
Evgeniy Shigartsov <evgeniy.shigartsov2@gmail.com>
2021-01-14 01:38:30 +0200

    clean src directory

--- React Homeworks/hm-2_online-shop/src/App.js
+++ React Homeworks/hm-2_online-shop/src/App.js
@@ -1,5 +1,4 @@
 import './App.scss'
-import Modal from './Components/Modal.jsx'
 import MainPage from './Components/MainPage.jsx'
 function App() {
     return <MainPage />
--- React Homeworks/hm-2_online-shop/src/index.js
+++ React Homeworks/hm-2_online-shop/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
+import './service-files/index.css'
 import App from './App'
-import reportWebVitals from './reportWebVitals'
+import reportWebVitals from './service-files/reportWebVitals'
 
 ReactDOM.render(
     <React.StrictMode>
